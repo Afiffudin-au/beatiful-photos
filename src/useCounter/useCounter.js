610d888e17ad6f2af6410b543920a48da3388e0e
afiffudin-au <afiffudin077@gmail.com>
2020-10-23 15:44:12 +0700

    remove the trash code

--- src/useCounter/useCounter.js
+++ src/useCounter/useCounter.js
@@ -2,14 +2,11 @@ import { useStateValue } from "../stateProvider/StateProvider"
 
 export const useCounterAddImage = ()=>{
   const [{},dispatch] = useStateValue()
-  const handleAddImages = (images)=>{
+  const addImages = (images)=>{
   dispatch({
       type : 'SET_IMAGE',
       images : images
     })
-  }
-  const addImages = (images)=>{
-    handleAddImages(images)
   }
   return[
     addImages
