44bbf41d2049cc86057cdb2c385d9c5ebf5bc0bb
afiffudin-au <afiffudin077@gmail.com>
2021-10-31 00:20:30 +0700

    update : tailwind

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import './assets/main.css'
-import { StateProvider } from './stateProvider/StateProvider';
-import * as serviceWorker from './serviceWorker';
-import reducer ,{initialState} from './reducer/Reducer'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { StateProvider } from './stateProvider/StateProvider'
+import * as serviceWorker from './serviceWorker'
+import reducer, { initialState } from './reducer/Reducer'
 ReactDOM.render(
-  <StateProvider reducer={reducer} initialState={initialState} >
+  <StateProvider reducer={reducer} initialState={initialState}>
     <App />
   </StateProvider>,
   document.getElementById('root')
-);
+)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
+serviceWorker.unregister()
