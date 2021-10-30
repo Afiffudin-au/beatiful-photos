878433017adeb2964e0ccc4e2067fb8e0adf6aa2
afiffudin-au <afiffudin077@gmail.com>
2021-10-30 21:47:51 +0700

    update

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,43 +1,82 @@
-import React from 'react'
-import { saveAs } from 'file-saver';
-import LazyLoad from 'react-lazyload';
-import LinearProgress from '@material-ui/core/LinearProgress';
-function Card({image}) {
-  const view = ()=>{
-    window.open(image.largeImageURL,'_blank');
+import React, { useState } from 'react'
+import { saveAs } from 'file-saver'
+import LazyLoad from 'react-lazyload'
+function Card({ image }) {
+  const [display, setDipslay] = useState('none')
+  const [imageLoaded, setImageLoaded] = useState(false)
+  const view = () => {
+    window.open(image.largeImageURL, '_blank')
   }
-  const donwload = ()=>{
-    saveAs(image.largeImageURL,'image')
+  const donwload = () => {
+    saveAs(image.largeImageURL, 'image')
+  }
+  const handleImageLoad = () => {
+    setImageLoaded(true)
+    setDipslay('block')
   }
   return (
-      <MemoizedChildComponent image={image} view={view} donwload={donwload}/>
+    <MemoizedChildComponent
+      display={display}
+      handleImageLoad={handleImageLoad}
+      imageLoaded={imageLoaded}
+      image={image}
+      view={view}
+      donwload={donwload}
+    />
   )
 }
-function ChildComponent({image,view,donwload}){
-  return(
-    <div className="card max-w-xs rounded shadow-2xl p-5 mx-2 my-2 bg-gray-300">
-        <LazyLoad 
-          width={150}
-          height={200}
-          debounce={false}
-          placeholder={<LinearProgress color="secondary"/>}
-          >
-          <img className="rounded -mt-10" src={image.webformatURL} alt=""/>
-         </LazyLoad>
-        <div className="flex flex-wrap mt-3 p-2">
-          <span className="py-1 px-2 bg-indigo-400 rounded mr-2 mb-2 text-gray-200">Likes : 533</span>
-          <span className="py-1 px-2 bg-indigo-400 rounded mr-2 mb-2 text-gray-200">Download : 232</span>
-          <span className="py-1 px-2 bg-indigo-400 rounded mr-2 mb-2 text-gray-200">Views : 232</span>
-        </div>
-        <div className="p-2">
-          <button onClick={view} target="_blank" className="text-sm py-2 px-3 rounded bg-blue-500 hover:bg-blue-600 outline-none shadow-lg mr-2 mb-2 text-gray-200 uppercase" rel="noopener noreferrer">View</button>
-          <button onClick={donwload} className="text-sm py-2 px-3 rounded bg-blue-500 hover:bg-blue-600 outline-none shadow-lg mr-2 mb-2 text-gray-200 uppercase">Download</button>
-        </div>
+function ChildComponent({
+  handleImageLoad,
+  imageLoaded,
+  display,
+  image,
+  view,
+  donwload,
+}) {
+  return (
+    <div className='card max-w-xs rounded shadow-2xl p-5 mx-2 my-2 bg-gray-300'>
+      <LazyLoad width={150} height={200} debounce={false}>
+        {!imageLoaded && (
+          <img className='rounded -mt-10' src='/285x185.png' alt='' />
+        )}
+        <img
+          className='rounded -mt-10 object-cover'
+          style={{ width: '285px', height: '185px', display: display }}
+          src={image.webformatURL}
+          alt=''
+          onLoad={handleImageLoad}
+        />
+      </LazyLoad>
+      <div className='flex flex-wrap mt-3 p-2'>
+        <span className='py-1 px-2 bg-indigo-400 rounded mr-2 mb-2 text-gray-200'>
+          Likes : 533
+        </span>
+        <span className='py-1 px-2 bg-indigo-400 rounded mr-2 mb-2 text-gray-200'>
+          Download : 232
+        </span>
+        <span className='py-1 px-2 bg-indigo-400 rounded mr-2 mb-2 text-gray-200'>
+          Views : 232
+        </span>
       </div>
+      <div className='p-2'>
+        <button
+          onClick={view}
+          target='_blank'
+          className='text-sm py-2 px-3 rounded bg-blue-500 hover:bg-blue-600 outline-none shadow-lg mr-2 mb-2 text-gray-200 uppercase'
+          rel='noopener noreferrer'>
+          View
+        </button>
+        <button
+          onClick={donwload}
+          className='text-sm py-2 px-3 rounded bg-blue-500 hover:bg-blue-600 outline-none shadow-lg mr-2 mb-2 text-gray-200 uppercase'>
+          Download
+        </button>
+      </div>
+    </div>
   )
 }
-function compare(prevProps , nextProps){
+function compare(prevProps, nextProps) {
   return JSON.stringify(prevProps) === JSON.stringify(nextProps)
 }
-const MemoizedChildComponent = React.memo(ChildComponent,compare)
+const MemoizedChildComponent = React.memo(ChildComponent, compare)
 export default Card
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,71 +1,85 @@
-import React,{useEffect,useRef,useCallback,useState} from 'react'
-import  Card  from '../card/Card'
+import React, { useEffect, useRef, useCallback, useState } from 'react'
+import Card from '../card/Card'
 import UseSearch from '../useSearch/UseSearch'
 import { useStateValue } from '../stateProvider/StateProvider'
 function Main() {
-  const [{images,paramsUrl},dispatch] = useStateValue()
-  const [pageNumber,setPageNumber] = useState(paramsUrl?.pageNumber)
-  const {error,loading,hasMore} = UseSearch()
+  const [{ images, paramsUrl }, dispatch] = useStateValue()
+  const [pageNumber, setPageNumber] = useState(paramsUrl?.pageNumber)
+  const { error, loading, hasMore } = UseSearch()
   const observer = useRef()
-  const lastImageElement = useCallback(node => {
-    if(loading) return 
-    if(observer.current) observer.current.disconnect()
-    observer.current = new IntersectionObserver(entries=>{
-      if(entries[0].isIntersecting && hasMore){
-       setPageNumber(pageNumber => pageNumber + 10)
-      }
-    })
-    if(node) observer.current.observe(node)
-  },[loading,hasMore])
-  useEffect(()=>{
+  const lastImageElement = useCallback(
+    (node) => {
+      if (loading) return
+      if (observer.current) observer.current.disconnect()
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((pageNumber) => pageNumber + 10)
+        }
+      })
+      if (node) observer.current.observe(node)
+    },
+    [loading, hasMore]
+  )
+  useEffect(() => {
     dispatch({
-      type : 'SET_PARAMS',
-      query : paramsUrl.query,
-      catagory : paramsUrl.catagory,
-      pageNumber : pageNumber
+      type: 'SET_PARAMS',
+      query: paramsUrl.query,
+      catagory: paramsUrl.catagory,
+      pageNumber: pageNumber,
     })
-  },[pageNumber,dispatch])
+  }, [pageNumber, dispatch])
 
-  useEffect(()=>{
+  useEffect(() => {
     setPageNumber(paramsUrl.pageNumber)
-  },[paramsUrl.pageNumber])
+  }, [paramsUrl.pageNumber])
   return (
-   <MemoizedChildComponent 
-    images={images}
-    lastImageElement={lastImageElement}
-    error={error}
-    loading={loading}
-   />
+    <MemoizedChildComponent
+      images={images}
+      lastImageElement={lastImageElement}
+      error={error}
+      loading={loading}
+    />
   )
 }
-function ChildComponent({images,lastImageElement,error,loading}){
+function ChildComponent({ images, lastImageElement, error, loading }) {
   return (
-    <div className="mt-12">
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 x2k:grid-cols-5 x4k:grid-cols-6">
-      {
-        images.map((image,index)=>{
-          if(images.length === index + 1){
-           return <div className="mx-auto mb-10" ref={lastImageElement} key={image.id}>
-             <Card  image={image}/> 
-           </div>
-          }else{
-            return <div className="mx-auto mb-10" key={image.id}>
-             <Card  image={image}/> 
-            </div>
+    <div className='mt-12'>
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 x2k:grid-cols-5 x4k:grid-cols-6'>
+        {images.map((image, index) => {
+          if (images.length === index + 1) {
+            return (
+              <div
+                className='mx-auto mb-10'
+                ref={lastImageElement}
+                key={image.id}>
+                <Card image={image} />
+              </div>
+            )
+          } else {
+            return (
+              <div className='mx-auto mb-10' key={image.id}>
+                <Card image={image} />
+              </div>
+            )
           }
-        })
-      }
+        })}
+      </div>
+      {!loading && images.length === 0 && (
+        <p className='max-w-2xl text-lg font-semibold mx-auto my-2 py-2 px-2 bg-blue-400 text-center rounded'>
+          No image result query...
+        </p>
+      )}
+      {error && (
+        <p className='max-w-2xl text-lg font-semibold mx-auto my-2 py-2 px-2 bg-red-700 text-center rounded'>
+          No Image Result...
+        </p>
+      )}
+      {loading && (
+        <p className='max-w-2xl text-lg font-semibold mx-auto my-2 py-2 px-2 bg-blue-400 text-center rounded'>
+          Loading...
+        </p>
+      )}
     </div>
-    {
-       !loading && images.length === 0 && <p className="max-w-2xl text-lg font-semibold mx-auto my-2 py-2 px-2 bg-blue-400 text-center rounded">No image result query...</p>
-    }
-    {
-      error && <p className="max-w-2xl text-lg font-semibold mx-auto my-2 py-2 px-2 bg-red-700 text-center rounded">No Image Result...</p>
-    }
-    {
-      loading && <p className="max-w-2xl text-lg font-semibold mx-auto my-2 py-2 px-2 bg-blue-400 text-center rounded">Loading...</p>
-    }
-  </div>
   )
 }
 const MemoizedChildComponent = React.memo(ChildComponent)
